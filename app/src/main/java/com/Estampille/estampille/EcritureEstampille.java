9ec0941cdad119cb8ea5a695a909b2fb6ff2fb7a
MrPomme <damien.tournie@edu.itescia.fr>
2020-06-23 10:15:27 +0200

    Début projet
    Début du projet Estampille

---
+++ app/src/main/java/com/Estampille/estampille/EcritureEstampille.java
@@ -0,0 +1,14 @@
+package com.Estampille.estampille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EcritureEstampille extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecriture_estampille);
+    }
+}
