b6cc75f6c23e463b19301d2b800600921cac0a3c
Yaelle <dubois.e1600202@eutd.univ-ubs.fr>
2020-10-19 09:43:12 +0200

    Delete useless import and add configuration for activities

--- app/src/main/java/istic/projet/estampille/MainActivity.java
+++ app/src/main/java/istic/projet/estampille/MainActivity.java
@@ -1,32 +1,17 @@
 package istic.projet.estampille;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.StrictMode;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.util.SparseIntArray;
-import android.view.Surface;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.work.Constraints;
@@ -34,57 +19,20 @@ import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-import androidx.viewpager2.widget.ViewPager2;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.text.Text;
-import com.google.mlkit.vision.text.TextRecognition;
-import com.google.mlkit.vision.text.TextRecognizer;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String errorFileCreate = "Error file create!";
-    private static final String errorConvert = "Error convert!";
-    private static final int REQUEST_IMAGE1_CAPTURE = 1;
-    protected String mCurrentPhotoPath;
-    ImageView firstImage;
-    TextView ocrText;
     int PERMISSION_ALL = 1;
-    boolean flagPermissions = false;
     String[] PERMISSIONS = {
             android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
             android.Manifest.permission.CAMERA
     };
-    private ProgressDialog mProgressDialog;
     private Context context;
-    private Uri photoURI1;
-    private Uri oldPhotoURI;
-    private Button ecrire;
-    private FloatingActionButton scanButton;
     private Toolbar mToolBar;
     private FragmentPagerAdapter fragmentPagerAdapter;
     private ViewPager viewPager;
@@ -160,8 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 .penaltyLog()
                 .build();
         StrictMode.setVmPolicy(builder);
-
-        //Check permission to create the OCR access
+        
     }
 
     private Data createInputDataForDownloadWorker() {
